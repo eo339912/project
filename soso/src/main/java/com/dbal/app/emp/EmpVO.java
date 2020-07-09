f91b1c7139fe999c8a80b04db288d0b39dac5844
eo339912 <eo339912@example.com>
2020-07-09 14:50:40 +0900

    0709

---
+++ soso/src/main/java/com/dbal/app/emp/EmpVO.java
@@ -0,0 +1,34 @@
+package com.dbal.app.emp;
+
+public class EmpVO {
+	String first_name;
+	String last_name;
+	String email;
+	String hiredate;
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getHiredate() {
+		return hiredate;
+	}
+	public void setHiredate(String hiredate) {
+		this.hiredate = hiredate;
+	}
+	
+	
+}
