2b49b147975dd02bda5546cf4628f3933a7b5b14
eo339912 <eo339912@example.com>
2020-07-14 15:12:38 +0900

    0714

---
+++ soso/src/main/java/com/soso/app/HomeController.java
@@ -0,0 +1,20 @@
+package com.soso.app;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home(Model model) {
+		return "home";
+	}
+	
+	
+}
