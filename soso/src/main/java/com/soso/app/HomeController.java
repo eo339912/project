cd95189933c8ecd557da98f4e5a55f38d2436aa9
eo339912 <eo339912@example.com>
2020-07-15 15:56:57 +0900

    0715

--- soso/src/main/java/com/soso/app/HomeController.java
+++ soso/src/main/java/com/soso/app/HomeController.java
@@ -1,22 +1,17 @@
 package com.soso.app;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.soso.app.menu.service.MenuService;
 import com.soso.app.menu.service.MenuVO;
+
 
 @Controller
 public class HomeController {
-	
-	@Autowired
-	MenuService menuServce;
 
 	@RequestMapping("/")
-	public String home(MenuVO menuVO) {
-		menuServce.getMenuList(menuVO);
+	public String home() {
 		return "home";
 	}
 	
