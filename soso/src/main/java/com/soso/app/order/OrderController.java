4032cc38ec18cfbb73f29a3c9edc0f9dd67c301a
eo339912 <eo339912@gmail.com>
2020-07-16 11:52:47 +0900

    0716

--- soso/src/main/java/com/soso/app/order/OrderController.java
+++ soso/src/main/java/com/soso/app/order/OrderController.java
@@ -14,30 +14,8 @@ public class OrderController {
 		return "order/orderInsert";
 	}
 	
-	//by혜원, 주문페이지 
-	@RequestMapping("/orderInsert2")
-	public String orderInsert2() {
-		return "order/orderInsert2";
-	}
 
 }
 
 
-
-//package com.soso.app.order;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//public class OrderController {
-//	
-//	
-//	//by혜원, 주문페이지 
-//	@RequestMapping("/orderInsert")
-//	public String orderInsert() {
-//		return "order/orderInsert";
-//	}
-//
-//}
 
