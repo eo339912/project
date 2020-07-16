5e19f3ef8107aead30705bf317777c19282c8fca
babuk2 <meditatif@naver.com>
2020-07-16 18:05:20 +0900

    fda

--- soso/src/main/java/com/soso/app/admin/web/AdminController.java
+++ soso/src/main/java/com/soso/app/admin/web/AdminController.java
@@ -1,6 +1,8 @@
 package com.soso.app.admin.web;
 
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
